330feb56ee385eaa877df353cdb5a51dbe1e4322
mcflugen <mcflugen@gmail.com>
2024-07-11 10:09:30 -0600

    use _d8_utils functions; some refactoring

--- landlab/components/priority_flood_flow_router/priority_flood_flow_router.py
+++ landlab/components/priority_flood_flow_router/priority_flood_flow_router.py
@@ -24,19 +24,15 @@ import numpy.matlib as npm
 from landlab import Component
 from landlab import FieldError
 from landlab import RasterModelGrid
-from landlab.components.priority_flood_flow_router._d8_flow_routing import (
-    _calc_slope_to_d8,
+from landlab.components.priority_flood_flow_router._d8_utils import (
+    accumulate_at_receiver_node,
 )
-from landlab.components.priority_flood_flow_router._d8_flow_routing import _find_d8_node
-from landlab.components.priority_flood_flow_router._d8_flow_routing import (
-    _find_receiver_d8_link,
+from landlab.components.priority_flood_flow_router._d8_utils import calc_slope_to_d8
+from landlab.components.priority_flood_flow_router._d8_utils import (
+    calc_steepest_d8_slope,
 )
-from landlab.components.priority_flood_flow_router._d8_flow_routing import (
-    _find_steepest_d8_neighbor_at_nodes,
-)
-from landlab.components.priority_flood_flow_router.cfuncs import _accumulate_flow_d8
-
-# from landlab.components.priority_flood_flow_router.cfuncs import _route_flow_d8
+from landlab.components.priority_flood_flow_router._d8_utils import find_d8_link
+from landlab.components.priority_flood_flow_router._d8_utils import find_d8_node
 from landlab.grid.nodestatus import NodeStatus
 from landlab.utils.return_array import return_array_at_node
 from landlab.utils.suppress_output import suppress_output
@@ -759,31 +755,36 @@ class PriorityFloodFlowRouter(Component):
         if (hill_flow and self._accumulate_flow_hill) or (
             not hill_flow and self._accumulate_flow
         ):
-            if np.any(~np.isclose(self.grid.at_node["water__unit_flux_in"], 1.0)):
-                dis = (
+            if hill_flow and self._accumulate_flow_hill:
+                save_drainage_area = self._hill_drainage_area
+                save_discharge = self._hill_discharges
+            else:
+                save_drainage_area = self._drainage_area
+                save_discharge = self._discharges
+
+            if np.allclose(self.grid.at_node["water__unit_flux_in"], 1.0):
+                discharge = self._node_cell_area
+            else:
+                discharge = (
                     self.grid.at_node["water__unit_flux_in"]
                     * self.grid.dx
                     * self.grid.dx
                 )
                 # Only core nodes (status == 0) need to receive a weight
-                dis[self.grid.status_at_node != NodeStatus.CORE] = 0.0
+                discharge[self.grid.status_at_node != NodeStatus.CORE] = 0.0
 
-            else:
-                dis = self._node_cell_area.copy()
-
-            da = self._node_cell_area.copy()
-            stack_flip = np.flip(self._sort)
-            # Filter out donors giving to receivers being -1
-            stack_flip = stack_flip[receiver_node_at_node[stack_flip] != -1]
-
-            _accumulate_flow_d8(da, dis, stack_flip, receiver_node_at_node)
-
-            if hill_flow and self._accumulate_flow_hill:
-                self._hill_drainage_area[:] = da
-                self._hill_discharges[:] = dis
-            else:
-                self._drainage_area[:] = da
-                self._discharges[:] = dis
+            accumulate_at_receiver_node(
+                self._node_cell_area,
+                receiver_node_at_node,
+                nodes=self._sort[::-1],
+                out=save_drainage_area,
+            )
+            accumulate_at_receiver_node(
+                discharge,
+                receiver_node_at_node,
+                nodes=self._sort[::-1],
+                out=save_discharge,
+            )
 
         # Closed nodes flow to themselves
         val = np.arange(receiver_node_at_node.shape[0])
@@ -910,7 +911,7 @@ class PriorityFloodFlowRouter(Component):
             raise ValueError(
                 "If hillslope properties are updated, the separate_hill_flow "
                 "property of the PriorityFloodFlowRouter class should be "
-                "True upon initialisation"
+                "True upon initialization"
             )
         self.calc_flow_dir_acc(hill_flow=True, update_depressions=update_depressions)
 
@@ -922,252 +923,3 @@ class PriorityFloodFlowRouter(Component):
             self.calc_flow_dir_acc(
                 hill_flow=True, update_depressions=self._update_hill_depressions
             )
-
-
-def calc_slope_to_d8(
-    grid,
-    z_at_node,
-    d8_neighbor_at_node,
-    nodes=None,
-):
-    """Calculate the slope between a node and one of its d8 neighbors.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from landlab import RasterModelGrid
-    >>> from landlab.components.priority_flood_flow_router.priority_flood_flow_router import (
-    ...     calc_slope_to_d8,
-    ... )
-
-    >>> grid = RasterModelGrid((3, 4), xy_spacing=(4.0, 3.0))
-    >>> z_at_node = [
-    ...     [0.0, 1.0, 2.0, 3.0],
-    ...     [0.0, 1.0, 2.0, 3.0],
-    ...     [0.0, 1.0, 2.0, 3.0],
-    ... ]
-    >>> neighbor_at_node = np.full(grid.number_of_nodes, 1)
-    >>> calc_slope_to_d8(grid, z_at_node, neighbor_at_node)
-    array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
-
-    >>> neighbor_at_node = np.full(grid.number_of_nodes, 0)
-    >>> calc_slope_to_d8(grid, z_at_node, neighbor_at_node).reshape(grid.shape)
-    array([[ 0.  ,  0.  ,  0.  ,  0.  ],
-           [ 0.  , -0.25, -0.25,  0.  ],
-           [ 0.  ,  0.  ,  0.  ,  0.  ]])
-
-    >>> neighbor_at_node = np.full(grid.number_of_nodes, 4)
-    >>> calc_slope_to_d8(grid, z_at_node, neighbor_at_node).reshape(grid.shape)
-    array([[ 0. ,  0. ,  0. ,  0. ],
-           [ 0. , -0.2, -0.2,  0. ],
-           [ 0. ,  0. ,  0. ,  0. ]])
-
-    >>> neighbor_at_node = np.full(grid.number_of_nodes, 6)
-    >>> calc_slope_to_d8(grid, z_at_node, neighbor_at_node, nodes=[5]).reshape(
-    ...     grid.shape
-    ... )
-    array([[0. , 0. , 0. , 0. ],
-           [0. , 0.2, 0. , 0. ],
-           [0. , 0. , 0. , 0. ]])
-    """
-    if nodes is None:
-        nodes = grid.core_nodes
-
-    slope_at_d8 = np.zeros(grid.number_of_nodes, dtype=float)
-
-    _calc_slope_to_d8(
-        grid.shape,
-        (grid.dx, grid.dy),
-        np.asarray(nodes),
-        np.asarray(z_at_node).reshape(-1),
-        np.asarray(d8_neighbor_at_node).reshape(-1),
-        slope_at_d8,
-    )
-
-    return slope_at_d8
-
-
-def find_d8_node(
-    grid,
-    d8_neighbor_at_node,
-    nodes=None,
-):
-    """Find d8 neighbor nodes.
-
-    Parameters
-    ----------
-    grid : RasterModelGrid
-        A Landlab grid.
-    d8_neighbor_at_node : array-like of int
-        A d8 neighbor for each grid node.
-    nodes : array-like of int, optional
-        Array of nodes on which to operate. The default is to operate
-        on all *core* nodes.
-
-    Examples
-    --------
-    >>> from landlab import RasterModelGrid
-    >>> from landlab.components.priority_flood_flow_router.priority_flood_flow_router import (
-    ...     find_d8_node,
-    ... )
-
-    >>> grid = RasterModelGrid((3, 4), xy_spacing=(4.0, 3.0))
-    >>> d8_neighbors_at_node = [
-    ...     [-1, -1, -1, -1],
-    ...     [-1, 7, 3, -1],
-    ...     [-1, -1, -1, -1],
-    ... ]
-    >>> find_d8_node(grid, d8_neighbors_at_node).reshape(grid.shape)
-    array([[-1, -1, -1, -1],
-           [-1,  2,  2, -1],
-           [-1, -1, -1, -1]])
-    """
-    if nodes is None:
-        nodes = grid.core_nodes
-
-    d8_node_at_node = np.full(grid.number_of_nodes, -1, dtype=int)
-    _find_d8_node(
-        grid.shape,
-        np.asarray(nodes),
-        np.asarray(d8_neighbor_at_node).reshape(-1),
-        d8_node_at_node,
-    )
-
-    return d8_node_at_node
-
-
-def find_d8_link(
-    grid,
-    d8_neighbor_at_node,
-    nodes=None,
-):
-    """Find links to d8 neighbors.
-
-    Parameters
-    ----------
-    grid : RasterModelGrid
-        A Landlab grid.
-    d8_neighbor_at_node : array-like of int
-        A d8 neighbor for each grid node.
-    nodes : array-like of int, optional
-        Array of nodes on which to operate. The default is to operate
-        on all *core* nodes.
-
-    Examples
-    --------
-    >>> from landlab import RasterModelGrid
-    >>> from landlab.components.priority_flood_flow_router.priority_flood_flow_router import (
-    ...     find_d8_link,
-    ... )
-
-    >>> grid = RasterModelGrid((3, 4), xy_spacing=(4.0, 3.0))
-    >>> d8_neighbors_at_node = [
-    ...     [-1, -1, -1, -1],
-    ...     [-1, 7, 3, -1],
-    ...     [-1, -1, -1, -1],
-    ... ]
-    >>> find_d8_link(grid, d8_neighbors_at_node).reshape(grid.shape)
-    array([[-1, -1, -1, -1],
-           [-1, 20,  5, -1],
-           [-1, -1, -1, -1]])
-    """
-    if nodes is None:
-        nodes = grid.core_nodes
-
-    d8_link_at_node = np.full(grid.number_of_nodes, -1, dtype=int)
-    _find_receiver_d8_link(
-        np.asarray(nodes),
-        np.asarray(d8_neighbor_at_node).reshape(-1),
-        grid.d8s_at_node,
-        d8_link_at_node,
-    )
-
-    return d8_link_at_node
-
-
-def calc_steepest_d8_slope(
-    grid,
-    z_at_node,
-    nodes=None,
-    is_active_node=None,
-    with_neighbors=False,
-):
-    """Find the steepest slope to a d8 neighbor.
-
-    Parameters
-    ----------
-    grid : RasterModelGrid
-        A Landlab grid.
-    z_at_node : array-like of float
-        Array of values at grid nodes.
-    nodes : array-like of int, optional
-        Array of nodes for which to calculate gradients. The default
-        is to operate on all *core* nodes.
-    is_active_node : array-like of bool
-        Array that indicates if a node is "active". Only slopes between
-        *active* nodes are considered. If not provided, all nodes are
-        considered *active*.
-    with_neighbors : bool, optional
-        If `True` return the corresponding d8 neighbor to which
-        the steepest slope was found.
-
-    Examples
-    --------
-    >>> from landlab import RasterModelGrid
-    >>> from landlab.components.priority_flood_flow_router.priority_flood_flow_router import (
-    ...     calc_steepest_d8_slope,
-    ... )
-
-    >>> grid = RasterModelGrid((3, 4), xy_spacing=(4.0, 3.0))
-    >>> z_at_node = [
-    ...     [0.0, 1.0, 5.0, 13.0],
-    ...     [0.0, 1.0, 5.0, 13.0],
-    ...     [0.0, 1.0, 5.0, 13.0],
-    ... ]
-    >>> calc_steepest_d8_slope(grid, z_at_node).reshape(grid.shape)
-    array([[0.  , 0.  , 0.  , 0.  ],
-           [0.  , 0.25, 1.  , 0.  ],
-           [0.  , 0.  , 0.  , 0.  ]])
-    >>> _, d8_neighbors = calc_steepest_d8_slope(grid, z_at_node, with_neighbors=True)
-    >>> d8_neighbors.reshape(grid.shape)
-    array([[-1, -1, -1, -1],
-           [-1,  2,  2, -1],
-           [-1, -1, -1, -1]])
-
-    >>> z_at_node = [
-    ...     [0.0, 15.0, 30.0, 30.0],
-    ...     [10.0, 30.0, 30.0, 30.0],
-    ...     [20.0, 30.0, 30.0, 30.0],
-    ... ]
-    >>> calc_steepest_d8_slope(grid, z_at_node).reshape(grid.shape)
-    array([[0., 0., 0., 0.],
-           [0., 6., 3., 0.],
-           [0., 0., 0., 0.]])
-    >>> _, d8_neighbors = calc_steepest_d8_slope(grid, z_at_node, with_neighbors=True)
-    >>> d8_neighbors.reshape(grid.shape)
-    array([[-1, -1, -1, -1],
-           [-1,  6,  6, -1],
-           [-1, -1, -1, -1]])
-    """
-    if nodes is None:
-        nodes = grid.core_nodes
-    if is_active_node is None:
-        is_active_node = grid.status_at_node != NodeStatus.CLOSED
-
-    d8_receiver_at_node = np.full(grid.number_of_nodes, -1, dtype=int)
-    steepest_slope = np.zeros(grid.number_of_nodes, dtype=float)
-
-    _find_steepest_d8_neighbor_at_nodes(
-        grid.shape,
-        (grid.dx, grid.dy),
-        np.asarray(nodes),
-        is_active_node.view(np.int8),
-        np.asarray(z_at_node).reshape(-1),
-        d8_receiver_at_node,
-        steepest_slope,
-    )
-
-    if with_neighbors:
-        return steepest_slope, d8_receiver_at_node
-    else:
-        return steepest_slope
